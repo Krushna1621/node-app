3f97c040dd6a0618495452365bc7828f0859b33f
Krushna1621 <shindekrushna825@gmail.com>
2023-07-30 16:32:19 +0530

    readfile

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-const http = require('http');
-
-function rqListener(req, res) {
-  // Set the response content type to HTML
-  res.setHeader('Content-Type', 'text/html');
-  
-  // Log your name to the console
-  console.log("Krushnakant");
-
-  const url = req.url;
-
-  // Check the URL and send the appropriate response
-  if (url === '/home') {
-    res.end("Welcome home");
-  } else if (url === '/about') {
-    res.end("Welcome to About Us page");
-  } else if (url === '/node') {
-    res.end("Welcome to my Node.js project");
-  } else {
-    // If the URL doesn't match any of the above cases, send a default response
-    res.end("Invalid URL");
-  }
-}
-
-// Create the server and listen on port 4000
-const server = http.createServer(rqListener);
-server.listen(4000, () => {
-  console.log('Server is running on port 4000.');
-});
+const http=require('http')
+const routes=require('./routes.js')
+const server=http.createServer(routes)
+server.listen(3000,()=>{
+  console.log("server runnig on 3000 port")
+})
--- writefile.js
+++ writefile.js
@@ -1,38 +1,51 @@
-const http=require('http')
-const fs=require('fs')
-function rqListener(req,res){
-    const url=req.url;
-    const method = req.method;
-    if(url==='/'){
-        res.write('<html>')
-        res.write('<head><title>Enter a message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-        res.end('</html>') 
-    }    
-    if(url === '/message' && method === 'POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk)
-            body.push(chunk) 
-        });
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split('=')[1];
-            fs.writeFileSync('hello.txt',message)
-        })
+// const http=require('http')
+// const fs=require('fs')
+// function rqListener(req,res){
+//     const url=req.url;
+//     const method = req.method;
+//     if(url==='/'){
+//         fs.readFile('hello.txt',{encoding:"utf-8"},(err,data)=>{
+//             if(err){
+//                 console.log(err)
+//             }
+//             else{
+//                 res.write('<html>')
+//                 res.write('<head><title>Enter a message</title></head>')
+//                 res.write(`<body>${data}</body>`);
+//                 res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+//                 res.end('</html>') 
+//             }
+//         })
+      
+//     }    
+//     if(url === '/message' && method === 'POST'){
+//         const body=[];
+//         req.on('data',(chunk)=>{
+//             console.log(chunk)
+//             body.push(chunk) 
+//         });
+//         req.on('end',()=>{
+//             const parsedBody=Buffer.concat(body).toString();
+//             const message=parsedBody.split('=')[1];
+//             fs.writeFileSync('hello.txt',message,(err)=>{
+//                 if(err){
+//                     console.log(err)
+//                 }
+//             })
+//         })
        
-        res.statusCode=302;
-        res.setHeader('Location','/')
-         return res.end()
-    }
+//         res.statusCode=302;
+//         res.setHeader('Location','/')
+//          return res.end()
+//     }
 
 
 
    
-}
+// }
 
 
-const server=http.createServer(rqListener)
-server.listen(4000,()=>{
-    console.log("server is runnig on 4000 port")
-})
+// const server=http.createServer(rqListener)
+// server.listen(4000,()=>{
+//     console.log("server is runnig on 4000 port")
+// })
